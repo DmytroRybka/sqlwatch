3fbbfaeb5176e2ab673685bac72ea6fee12dac32
dmitry s mamonv <dmitry.s.mamonv@gmail.com>
2011-01-20 23:31:24 +0300

    demo extended to expose transaction usage

--- sqlwatchdemo/src/main/java/sqlwatch4/demo/DemoMain.java
+++ sqlwatchdemo/src/main/java/sqlwatch4/demo/DemoMain.java
@@ -4,28 +4,50 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import javax.transaction.TransactionRolledbackException;
 import java.util.Random;
 
 /**
  * @author dmitry.mamonov
  */
 
-public class Main {
+public class DemoMain {
     static Random rnd = new Random();
 
     public static void main(String[] args) {
         try {
             ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
-            JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+            final JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+            TransactionTemplate transactionTemplate = applicationContext.getBean(TransactionTemplate.class);
             jdbcTemplate.update("CREATE TABLE client(id INT, name VARCHAR(50), age INT, sex CHAR)");
 
             while (true) {
-                int loopInsert = rnd.nextInt(5);
-                for (int i = 0; i < loopInsert; i++) {
-                    randomInsert(jdbcTemplate);
-                    Thread.sleep(40);
+                try {
+                    transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+                        @Override
+                        protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                            int loopInsert = rnd.nextInt(5);
+                            for (int i = 0; i < 3+loopInsert; i++) {
+                                randomInsert(jdbcTemplate);
+                                try {
+                                    Thread.sleep(40);
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            if (rnd.nextBoolean()) {
+                                throw new RuntimeException("Opps! Rollback!");
+                            }
+                        }
+                    });
+                } catch (RuntimeException oops) {
+                    System.out.println(oops.getMessage());
                 }
+
                 Thread.sleep(3000);
                 int loopSelect = rnd.nextInt(12);
                 for (int i = 0; i < loopSelect; i++) {
